d57d79fdcb543bc1b2c66e2ca0ddf1df0e00abbb
Isaac Haws <ighaws2005@gmail.com>
2023-10-24 10:29:25 -0500

    Finished W7 learning activitys

---
+++ lesson07/scripts/main.js
@@ -0,0 +1,6 @@
+
+let currentYear = new Date().getFullYear();
+document.querySelector('#year').innerHTML = currentYear;
+
+let lastModified = document.lastModified;
+document.querySelector("#lastModified").innerHTML = lastModified;
