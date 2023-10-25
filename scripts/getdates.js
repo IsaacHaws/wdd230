e9a0b4468b2ffcb3efe86d4922814077c559a39a
Isaac Haws <ighaws2005@gmail.com>
2023-10-25 09:31:47 -0500

    Finished W7 home page enhancment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,3 +7,17 @@ let lastModified = document.lastModified;
 document.querySelector("#lastModifided").innerText = "Last Modification: " + lastModified;
 
 
+const pageVisits = document.querySelector(".pageVisits");
+
+let numVisits = Number(localStorage.getItem("numVisits")) || 0;
+
+if (numVisits !== 0) {
+    pageVisits.textContent = "Page Visits: " + numVisits;
+}
+else {
+    pageVisits.textContent = "Page Visits: This is your first time!!";
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits", numVisits)
