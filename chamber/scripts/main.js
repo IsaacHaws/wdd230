ddd0ffcd5acaf9257e6e3e2acdfbe6761c82220b
Isaac Haws <ighaws2005@gmail.com>
2023-10-28 17:34:08 -0500

    Finished discover page

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -10,3 +10,44 @@ menuButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     menuButton.classList.toggle("open")
 })
+
+
+const visitDateMsg = document.querySelector("#latestVisit");
+
+//Milliseconds to days convertion rate
+const msToDays = 84600000;
+
+//Get the last visit date from local storage
+let lastVisitDate = Number(localStorage.getItem("visitDate")) || 0;
+
+//If the user has never visited display welcome message
+if (lastVisitDate == 0) {
+    visitDateMsg.innerHTML = "Welcome! Let us know if yopu have any questions";
+}
+
+//If user has visited before execute else statment
+else {
+    //Calculate how many days it has been since the user visited
+    let daysAgo = ((lastVisitDate - Date.now()) / msToDays).toFixed(0);
+
+    //If the user has visited less than a day ago dispaly message
+    if (daysAgo < 1) {
+        visitDateMsg.innerHTML = "Back so soon! Awsome!";
+    }
+    else {
+        //If-Else statment to determine whether to add a "s" to the word "day" 
+        if(daysAgo == 1) {
+            visitDateMsg.innerHTML = "You last visited " + daysAgo + " day ago!";
+        }
+        else {
+            visitDateMsg.innerHTML = "You last visited " + daysAgo + " days ago!";
+        }
+        
+    }
+}
+
+//Update local storage on last visited date
+localStorage.setItem("visitDate", Date.now());
+
+//Debbugging line of code to force If-Else statments
+//localStorage.setItem("visitDate", 1698765360023);
