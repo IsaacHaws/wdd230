4e625e8963d5c7084accc3431845cc9a6cb0082c
Isaac Haws <ighaws2005@gmail.com>
2023-11-13 11:53:20 -0600

    Finished W10 Learning Activitys

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&units=imperial&appid={21a3313f9a9f14adccefb2f843027eb5}";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=21a3313f9a9f14adccefb2f843027eb5";
 
 
 async function apiFetch() {
@@ -11,6 +11,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            displayResults(data);
         }
         else {
             throw Error(await response.text());
@@ -18,6 +19,16 @@ async function apiFetch() {
     } catch (error) {
         console.log(error);
     }
+}
+
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
 }
 
 
