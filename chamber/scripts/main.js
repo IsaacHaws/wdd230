4a238295dde1852da6621d3325b2615c2fa8a949
Isaac Haws <ighaws2005@gmail.com>
2023-11-17 22:12:08 -0600

    W10 assignment

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -12,42 +12,23 @@ menuButton.addEventListener("click", () => {
 })
 
 
-const visitDateMsg = document.querySelector("#latestVisit");
 
-//Milliseconds to days convertion rate
-const msToDays = 84600000;
+const banner = document.querySelector("#banner");
+const bannerBnt = document.querySelector("#bannerBnt");
 
-//Get the last visit date from local storage
-let lastVisitDate = Number(localStorage.getItem("visitDate")) || 0;
+bannerBnt.addEventListener("click", () => {
+    banner.classList.toggle("close");
+    navigation.classList.toggle("close");
+})
 
-//If the user has never visited display welcome message
-if (lastVisitDate == 0) {
-    visitDateMsg.innerHTML = "Welcome! Let us know if yopu have any questions";
+
+let dayOfWeek = new Date().getDay()
+
+dayOfWeek = 2;
+
+if ((dayOfWeek == 1) || (dayOfWeek == 2) || (dayOfWeek == 3)) {
+    
 }
-
-//If user has visited before execute else statment
 else {
-    //Calculate how many days it has been since the user visited
-    let daysAgo = ((lastVisitDate - Date.now()) / msToDays).toFixed(0);
-
-    //If the user has visited less than a day ago dispaly message
-    if (daysAgo < 1) {
-        visitDateMsg.innerHTML = "Back so soon! Awsome!";
-    }
-    else {
-        //If-Else statment to determine whether to add a "s" to the word "day" 
-        if(daysAgo == 1) {
-            visitDateMsg.innerHTML = "You last visited " + daysAgo + " day ago!";
-        }
-        else {
-            visitDateMsg.innerHTML = "You last visited " + daysAgo + " days ago!";
-        }
-        
-    }
+    banner.style.display = "none";
 }
-
-//Update local storage on last visited date
-localStorage.setItem("visitDate", Date.now());
-
-//Debbugging line of code to force If-Else statments
-//localStorage.setItem("visitDate", 1698765360023);
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,96 @@
+const url = "https://api.openweathermap.org/data/2.5/forecast?zip=76571,us&units=imperial&cnt=5&appid=407e6ab59006e88662f16686d1960fdf";
+
+const currentIcon = document.querySelector("#weatherIcon");
+const currentTemp = document.querySelector("#temp");
+const currentDiscription = document.querySelector("#weatherDiscription");
+
+const futureWeather = document.querySelector(".futureWeather");
+
+
+async function getWeatherData() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayWeather(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayWeather(data) {
+    const iconSrc = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+    const iconAlt = data.list[0].weather[0].description;
+    currentIcon.setAttribute("src", iconSrc);
+    currentIcon.setAttribute("alt", iconAlt);
+
+    currentTemp.innerHTML = `${data.list[0].main.temp}&#8457;`;
+    currentDiscription.innerHTML = iconAlt.charAt(0).toUpperCase() + iconAlt.slice(1);
+
+    data.list.forEach((event) => {
+        let weatherCard = document.createElement("section");
+        let weekDay = document.createElement("h4");
+        let weatherIcon = document.createElement("img");
+
+        let maxTemp = document.createElement("p")
+        let minTemp = document.createElement("p")
+
+        let timeStamp = event.dt_txt;
+        let dayOfWeek = new Date(timeStamp).getDay()
+
+        switch (dayOfWeek) {
+            case 0:
+                dayOfWeek = "Sun";
+                break;
+
+            case 1:
+                dayOfWeek = "Mon";
+                break;
+            
+            case 2:
+                dayOfWeek = "Tues";
+                break;
+            
+            case 3: 
+                dayOfWeek = "Wed";
+                break;
+
+            case 4: 
+                dayOfWeek = "Thurs";
+                break;
+
+            case 5: 
+                dayOfWeek = "Fri";
+                break;
+
+            case 6:
+                dayOfWeek = "Sat";
+        
+            default:
+                break;
+        }
+        weekDay.innerHTML = dayOfWeek;
+        
+        const imgSrc = `https://openweathermap.org/img/w/${event.weather[0].icon}.png`;
+        const imgAlt = event.weather[0].description;
+        weatherIcon.setAttribute("src", imgSrc);
+        weatherIcon.setAttribute("alt", imgAlt);
+
+        maxTemp.innerHTML = `${event.main.temp_max.toFixed(0)}&#8457;`;
+        minTemp.innerHTML = `${event.main.temp_min.toFixed(0)}&#8457;`;
+
+        weatherCard.appendChild(weekDay);
+        weatherCard.appendChild(weatherIcon);
+        weatherCard.appendChild(maxTemp);
+        weatherCard.appendChild(minTemp);
+
+        futureWeather.appendChild(weatherCard);
+    });
+}
+
+getWeatherData();
